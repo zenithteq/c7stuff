9dd9686adff22421ad22668c68e980eff6e0480f
zenithteq <jahan@zenithteq.com>
2016-03-27 18:04:37 +0400

    Update c7prereqs.sh

--- c7prereqs.sh
+++ c7prereqs.sh
@@ -1,1 +1,33 @@
+#Update CentOS
+yum update -y
 
+#Disable SELinux
+setenforce 0
+sed -i "s|SELINUX=enforcing|SELINUX=disabled|" /etc/selinux/config
+
+#Install EPEL
+yum install epel-release -y
+
+#Install useful tools
+yum install vim nano wget -y
+
+#Install network monitoring tools
+yum install net-tools iptraf iftop mtr -y
+
+#Install Vmware Tools (Open VM Tools)
+#yum install open-vm-tools -y
+#systemctl start vmtoolsd.service
+#systemctl enable vmtoolsd.service
+
+#Disable firewalld
+systemctl disable firewalld
+systemctl mask firewalld.service
+systemctl stop firewalld.service
+
+#Install iptables (optional)
+yum install iptables-services -y
+systemctl enable iptables.service
+systemctl start iptables.service
+
+#Reboot
+reboot
