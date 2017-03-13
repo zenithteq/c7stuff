2fa2db339a61fb4c3497aa155878013e905af8dc
zenithteq <jahan@zenithteq.com>
2017-03-13 20:16:18 +0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ sed -i "s|SELINUX=enforcing|SELINUX=disabled|" /etc/selinux/config
 #read -p "Do you want to change the hostname? [y/n]: " -e -i n NEW_HOSTNAME
 HOSTNAME=$(hostname)
 NEW_HOSTNAME="$1"
-IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+IP=$(wget -4qO- "http://whatismyip.akamai.com/")
 clear
 echo "Current IP is:" $IP
 echo "Current hostname is:" $HOSTNAME
@@ -20,7 +20,7 @@ fi
 echo ""
 if [ -z "$NEW_HOSTNAME" ]; then
  echo "Error: no hostname entered. Exiting."
-# exit 1
+ exit 1
 fi
 echo "Changing hostname from $HOSTNAME to $NEW_HOSTNAME..."
 hostnamectl set-hostname $NEW_HOSTNAME
