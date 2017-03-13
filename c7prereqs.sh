080c9f6e5c6b12d61ebf3d5f5a698f6ecfdc82c0
zenithteq <jahan@zenithteq.com>
2017-03-13 22:27:24 +0400

    Update c7prereqs.sh

--- c7prereqs.sh
+++ c7prereqs.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+#Change hostname
+HOSTNAME=$(hostname)
+NEW_HOSTNAME="$1"
+IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+if [[ "$IP" = "" ]]; then
+		IP=$(wget -4qO- "http://whatismyip.akamai.com/")
+fi
+clear
+echo "Current IP is:" $IP
+echo "Current hostname is:" $HOSTNAME
+echo ""
+read -p "Do you want to change the hostname? [y/n]: " -e -i n NEW_HOSTNAME
+if [ "$NEW_HOSTNAME" = 'y' ]; then
+ echo -n "Please enter new hostname: "
+ read NEW_HOSTNAME < /dev/tty
+echo ""
+echo "Changing hostname from $HOSTNAME to $NEW_HOSTNAME..."
+hostnamectl set-hostname $NEW_HOSTNAME
+echo "Done."
+else
+  echo "hostname unchanged"
+fi
+
+#Install Webmin part 1
+clear
+read -p "Do you want to install Webmin? [y/n]: " -e -i n INSTALL
+
 #Update CentOS
 yum update -y
 
@@ -36,6 +63,15 @@ yum install iptables-services -y
 systemctl enable iptables.service
 systemctl start iptables.service
 
+#Install Webmin part 2
+if [[ "$INSTALL" = 'y' ]]; then
+  wget http://www.webmin.com/download/rpm/webmin-current.rpm
+  yum install perl perl-Net-SSLeay openssl perl-IO-Tty -y
+  rpm -U webmin-current.rpm
+  iptables -I INPUT -p tcp -m tcp --dport 10000 -j ACCEPT
+  service iptables save
+fi
+
 #Install Fail2Ban
 yum install fail2ban -y
 systemctl enable fail2ban
@@ -48,41 +84,8 @@ banaction = iptables-multiport
 enabled = true" > /etc/fail2ban/jail.local
 systemctl restart fail2ban
 
-#Install Webmin
-clear
-read -p "Do you want to install Webmin? [y/n]: " -e -i n INSTALL
- if [[ "$INSTALL" = 'y' ]]; then
-  wget http://www.webmin.com/download/rpm/webmin-current.rpm
-  yum install perl perl-Net-SSLeay openssl perl-IO-Tty -y
-  rpm -U webmin-current.rpm
-  iptables -I INPUT -p tcp -m tcp --dport 10000 -j ACCEPT
-  service iptables save
- fi
-
 #Update CentOS - redo
 yum update -y
-
-#Change hostname
-read -p "Do you want to change the hostname? [y/n]: " -e -i n NEW_HOSTNAME
-HOSTNAME=$(hostname)
-NEW_HOSTNAME="$1"
-IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
-clear
-echo "Current IP is:" $IP
-echo "Current hostname is:" $HOSTNAME
-echo ""
-if [ -z "$NEW_HOSTNAME" ]; then
- echo -n "Please enter new hostname: "
- read NEW_HOSTNAME < /dev/tty
-fi
-echo ""
-#if [ -z "$NEW_HOSTNAME" ]; then
-# echo "Error: no hostname entered. Exiting."
-# exit 1
-#fi
-echo "Changing hostname from $HOSTNAME to $NEW_HOSTNAME..."
-hostnamectl set-hostname $NEW_HOSTNAME
-echo "Done."
 
 #Install Yubico
 yum install pam_yubico -y
@@ -92,4 +95,4 @@ sed -i 's/^#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 sed -i 's/^#Protocol 2/Protocol 2/' /etc/ssh/sshd_config
 
 #Reboot
-#reboot
+reboot
