7256840455eae6b26fe55613f1f880414de4b813
zenithteq <jahan@zenithteq.com>
2017-03-13 21:10:24 +0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,66 @@
 #!/bin/bash
 
+#Update CentOS
+yum update -y
+
 #Disable SELinux
 setenforce 0
 sed -i "s|SELINUX=enforcing|SELINUX=disabled|" /etc/selinux/config
+
+#Install EPEL
+yum install epel-release -y
+
+#Install useful tools
+yum install vim nano wget unzip rsync -y
+
+#Install monitoring tools
+yum install net-tools iptraf iftop mtr iperf bind-utils dstat -y
+
+##Install NTP
+yum install ntp -y
+systemctl start ntpd
+systemctl enable ntpd
+
+#Install Vmware Tools (Open VM Tools)
+#yum install open-vm-tools -y
+#systemctl start vmtoolsd.service
+#systemctl enable vmtoolsd.service
+
+#Disable firewalld
+systemctl disable firewalld
+systemctl mask firewalld.service
+systemctl stop firewalld.service
+
+#Install iptables
+yum install iptables-services -y
+systemctl enable iptables.service
+systemctl start iptables.service
+
+#Install Fail2Ban
+yum install fail2ban -y
+systemctl enable fail2ban
+echo "[DEFAULT]
+# Ban hosts for one hour:
+bantime = 3600
+# Override /etc/fail2ban/jail.d/00-firewalld.conf:
+banaction = iptables-multiport
+[sshd]
+enabled = true" > /etc/fail2ban/jail.local
+systemctl restart fail2ban
+
+#Install Webmin
+clear
+read -p "Do you want to install Webmin? [y/n]: " -e -i n INSTALL
+ if [[ "$INSTALL" = 'y' ]]; then
+  wget http://www.webmin.com/download/rpm/webmin-current.rpm
+  yum install perl perl-Net-SSLeay openssl perl-IO-Tty -y
+  rpm -U webmin-current.rpm
+  iptables -I INPUT -p tcp -m tcp --dport 10000 -j ACCEPT
+  service iptables save
+fi
+
+#Update CentOS - redo
+yum update -y
 
 #Change hostname
 HOSTNAME=$(hostname)
@@ -19,7 +77,6 @@ read -p "Do you want to change the hostname? [y/n]: " -e -i n NEW_HOSTNAME
 if [ "$NEW_HOSTNAME" = 'y' ]; then
  echo -n "Please enter new hostname: "
  read NEW_HOSTNAME < /dev/tty
-#fi
 echo ""
 echo "Changing hostname from $HOSTNAME to $NEW_HOSTNAME..."
 hostnamectl set-hostname $NEW_HOSTNAME
@@ -27,6 +84,10 @@ echo "Done."
 else
   echo "hostname unchanged"
 fi
+
+#Install Yubico
+yum install pam_yubico -y
+
 #Secure SSH
 sed -i 's/^#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 sed -i 's/^#Protocol 2/Protocol 2/' /etc/ssh/sshd_config
