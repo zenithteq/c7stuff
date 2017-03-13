6c27d7314f57509371600695bed27d0cbcb93853
zenithteq <jahan@zenithteq.com>
2017-03-13 21:43:27 +0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+#Change hostname
+HOSTNAME=$(hostname)
+NEW_HOSTNAME="$1"
+IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+if [[ "$IP" = "" ]]; then
+		IP=$(wget -4qO- "http://whatismyip.akamai.com/")
+fi
+clear
+echo "Current IP is:" $IP
+echo "Current hostname is:" $HOSTNAME
+echo ""
+read -p "Do you want to change the hostname? [y/n]: " -e -i n NEW_HOSTNAME
+if [ "$NEW_HOSTNAME" = 'y' ]; then
+ echo -n "Please enter new hostname: "
+ read NEW_HOSTNAME < /dev/tty
+echo ""
+echo "Changing hostname from $HOSTNAME to $NEW_HOSTNAME..."
+hostnamectl set-hostname $NEW_HOSTNAME
+echo "Done."
+else
+  echo "hostname unchanged"
+fi
+
+#Install Webmin
+clear
+read -p "Do you want to install Webmin? [y/n]: " -e -i n INSTALL
+
 #Update CentOS
 yum update -y
 
@@ -36,6 +63,15 @@ yum install iptables-services -y
 systemctl enable iptables.service
 systemctl start iptables.service
 
+#Install Webmin
+if [[ "$INSTALL" = 'y' ]]; then
+  wget http://www.webmin.com/download/rpm/webmin-current.rpm
+  yum install perl perl-Net-SSLeay openssl perl-IO-Tty -y
+  rpm -U webmin-current.rpm
+  iptables -I INPUT -p tcp -m tcp --dport 10000 -j ACCEPT
+  service iptables save
+fi
+
 #Install Fail2Ban
 yum install fail2ban -y
 systemctl enable fail2ban
@@ -48,42 +84,8 @@ banaction = iptables-multiport
 enabled = true" > /etc/fail2ban/jail.local
 systemctl restart fail2ban
 
-#Install Webmin
-clear
-read -p "Do you want to install Webmin? [y/n]: " -e -i n INSTALL
- if [[ "$INSTALL" = 'y' ]]; then
-  wget http://www.webmin.com/download/rpm/webmin-current.rpm
-  yum install perl perl-Net-SSLeay openssl perl-IO-Tty -y
-  rpm -U webmin-current.rpm
-  iptables -I INPUT -p tcp -m tcp --dport 10000 -j ACCEPT
-  service iptables save
-fi
-
 #Update CentOS - redo
 yum update -y
-
-#Change hostname
-HOSTNAME=$(hostname)
-NEW_HOSTNAME="$1"
-IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-if [[ "$IP" = "" ]]; then
-		IP=$(wget -4qO- "http://whatismyip.akamai.com/")
-fi
-clear
-echo "Current IP is:" $IP
-echo "Current hostname is:" $HOSTNAME
-echo ""
-read -p "Do you want to change the hostname? [y/n]: " -e -i n NEW_HOSTNAME
-if [ "$NEW_HOSTNAME" = 'y' ]; then
- echo -n "Please enter new hostname: "
- read NEW_HOSTNAME < /dev/tty
-echo ""
-echo "Changing hostname from $HOSTNAME to $NEW_HOSTNAME..."
-hostnamectl set-hostname $NEW_HOSTNAME
-echo "Done."
-else
-  echo "hostname unchanged"
-fi
 
 #Install Yubico
 yum install pam_yubico -y
