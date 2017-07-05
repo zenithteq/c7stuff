2d131820b497b0535c36ba553bc30b32a00a9e89
zenithteq <jahan@zenithteq.com>
2017-07-05 11:02:38 +0400

    Update c7prereqs.sh

--- c7prereqs.sh
+++ c7prereqs.sh
@@ -63,11 +63,15 @@ else
 fi
 
 #Install open-vm-tools
-if dmidecode -s system-product-name | grep VMware > /dev/null; then
+if rpm -q open-vm-tools > /dev/null; then
+  echo "Package open-vm-tools is already installed."; 
+elif dmidecode -s system-product-name | grep VMware > /dev/null; then
   echo "You are running on VMware. Open VM Tools will now be installed.";
   yum install open-vm-tools -y
   systemctl start vmtoolsd.service
   systemctl enable vmtoolsd.service
+else
+  yum install open-vm-tools -y
 fi
 
 #Disable firewalld
