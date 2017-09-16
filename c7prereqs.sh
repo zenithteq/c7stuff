54cf077151f8b658e0a0db40e05d427355e51b31
zenithteq <jahan@zenithteq.com>
2017-09-16 21:01:05 +0400

    Changes to install of packages and echo
    some more info during install but the yum install done quietly.

--- c7prereqs.sh
+++ c7prereqs.sh
@@ -33,24 +33,32 @@ read -p "Do you want to install Webmin? [y/n]: " -e -i n WEBMININSTALL
 sleep 5
 
 #Update CentOS
-yum update -y
+echo ""
+echo "Now updating CentOS"
+yum update -y -q
 
 #Disable SELinux
+echo ""
+echo "Disabling SELinux"
 setenforce 0
 sed -i "s|SELINUX=enforcing|SELINUX=disabled|" /etc/selinux/config
 
 #Install EPEL
 if rpm -q epel-release > /dev/null; then
   echo "Package epel-release is already installed.";
-else 
-  yum install epel-release -y
+else
+  echo ""
+  echo "Now installing epel"
+  yum install epel-release -y -q
 fi
 
 #Install ntp
 if rpm -q ntp > /dev/null; then
   echo "Package ntp is already installed."; 
 else
-  yum install ntp -y
+  echo ""
+  echo "Now installing ntp"
+  yum install ntp -y -q
   systemctl start ntpd
   systemctl enable ntpd
 fi
@@ -60,7 +68,7 @@ if dmidecode -s system-product-name | grep VMware > /dev/null; then
 if rpm -q open-vm-tools > /dev/null; then
   echo "You are running on VMware. Package open-vm-tools is already installed.";
 else echo "You are running on VMware. Package open-vpm-tools will now be installed.";
-  yum install open-vm-tools -y
+  yum install open-vm-tools -y -q
   systemctl start vmtoolsd.service
   systemctl enable vmtoolsd.service
 fi
@@ -77,7 +85,9 @@ systemctl stop firewalld.service
 if rpm -q iptables-services > /dev/null; then
   echo "Package iptables-services is already installed."; 
 else
-  yum install iptables-services -y
+  echo ""
+  echo "Now installing iptables"
+  yum install iptables-services -y -q
   systemctl enable iptables.service
   systemctl start iptables.service
 fi
@@ -86,7 +96,9 @@ fi
 if rpm -q fail2ban > /dev/null; then
   echo "Package fail2ban is already installed."; 
 else
-  yum install fail2ban -y
+  echo ""
+  echo "Now installing fail2ban"
+  yum install fail2ban -y -q
   systemctl enable fail2ban
 echo "[DEFAULT]
 # Ban hosts for one hour:
@@ -103,7 +115,9 @@ fi
 if rpm -q pam_yubico > /dev/null; then
   echo "Package pam_yubico is already installed."; 
 else
-  yum install pam_yubico -y
+  echo ""
+  echo "Now installing Yubico"
+  yum install pam_yubico -y -q
 fi
 
 #Secure SSH
@@ -114,91 +128,117 @@ sed -i 's/^#Protocol 2/Protocol 2/' /etc/ssh/sshd_config
 if rpm -q vim-enhanced > /dev/null; then
   echo "Package vim is already installed."; 
 else
-  yum install vim -y
+  echo ""
+  echo "Now installing vim"
+  yum install vim -y -q
 fi
 
 #Install nano
 if rpm -q nano > /dev/null; then
   echo "Package nano is already installed."; 
 else
-  yum install nano -y
+  echo ""
+  echo "Now installing nano"
+  yum install nano -y -q
 fi
 
 #Install wget
 if rpm -q wget > /dev/null; then
   echo "Package wget is already installed."; 
 else
-  yum install wget -y
+  echo ""
+  echo "Now installing wget"
+  yum install wget -y -q
 fi
 
 #Install unzip
 if rpm -q unzip > /dev/null; then
   echo "Package unzip is already installed."; 
 else
-  yum install unzip -y
+  echo ""
+  echo "Now installing unzip"
+  yum install unzip -y -q
 fi
 
 #Install screen
 if rpm -q screen > /dev/null; then
   echo "Package screen is already installed."; 
 else
-  yum install screen -y
+  echo ""
+  echo "Now installing screen"
+  yum install screen -y -q
 fi
 
 #Install rsync
 if rpm -q rsync > /dev/null; then
   echo "Package rsync is already installed."; 
 else
-  yum install rsync -y
+  echo ""
+  echo "Now installing rsync"
+  yum install rsync -y -q
 fi
 
 #Install net-tools
 if rpm -q net-tools > /dev/null; then
   echo "Package net-tools is already installed."; 
 else
-  yum install net-tools -y
+  echo ""
+  echo "Now installing net-tools"
+  yum install net-tools -y -q
 fi
 
 #Install iptraf
 if rpm -q iptraf-ng > /dev/null; then
   echo "Package iptraf is already installed."; 
 else
-  yum install iptraf -y
+  echo ""
+  echo "Now installing iptraf"
+  yum install iptraf -y -q
 fi
 
 #Install iftop
 if rpm -q iftop > /dev/null; then
   echo "Package iftop is already installed."; 
 else
-  yum install iftop -y
+  echo ""
+  echo "Now installing iftop"
+  yum install iftop -y -q
 fi
 
 #Install mtr
 if rpm -q mtr > /dev/null; then
   echo "Package mtr is already installed."; 
 else
-  yum install mtr -y
+  echo ""
+  echo "Now installing mtr"
+  yum install mtr -y -q
 fi
 
 #Install iperf
 if rpm -q iperf > /dev/null; then
   echo "Package iperf is already installed."; 
 else
-  yum install iperf -y
+  echo ""
+  echo "Now installing iperf"
+  yum install iperf -y -q
 fi
 
 #Install bind-utils
 if rpm -q bind-utils > /dev/null; then
   echo "Package bind-utils is already installed."; 
 else
-  yum install bind-utils -y
+  echo ""
+  echo "Now installing bind-utils"
+  yum install bind-utils -y -q
 fi
 
 #Install dstat
 if rpm -q dstat > /dev/null; then
   echo "Package dstat is already installed."; 
 else
-  yum install dstat -y
+  echo ""
+  echo "Now installing dstat"
+  yum install dstat -y -q
 fi
 
 #Install Webmin part 2
@@ -210,7 +250,7 @@ then
   else	
   	echo -e "\e[1;31mWebmin will now be installed.\e[0m"
 	wget http://www.webmin.com/download/rpm/webmin-current.rpm
-    	yum install perl perl-Net-SSLeay openssl perl-IO-Tty perl-Encode-Detect -y
+    	yum install perl perl-Net-SSLeay openssl perl-IO-Tty perl-Encode-Detect -y -q
     	rpm -U webmin-current.rpm
     	iptables -I INPUT -p tcp -m tcp --dport 10000 -j ACCEPT
     	service iptables save
@@ -225,7 +265,7 @@ else
 fi
 
 #Update CentOS - redo
-yum update -y
+yum update -y -q
 
 #Reboot
 #echo "This host will now reboot."
