ac9b3d2241fbad37f966e255425cd2e4c2c65c95
zenithteq <jahan@zenithteq.com>
2018-10-07 07:39:58 +0400

    Minor echo fixes

--- c7prereqs.sh
+++ c7prereqs.sh
@@ -68,6 +68,7 @@ fi
 #Install open-vm-tools
 if dmidecode -s system-product-name | grep VMware > /dev/null; then
 if rpm -q open-vm-tools > /dev/null; then
+  echo ""
   echo "You are running on VMware. Package open-vm-tools is already installed.";
 else echo "You are running on VMware. Package open-vm-tools will now be installed.";
   yum install open-vm-tools -y -q
@@ -75,21 +76,26 @@ else echo "You are running on VMware. Package open-vm-tools will now be installe
   systemctl enable vmtoolsd.service
 fi
 else
+ echo ""
  echo "You are not running on VMware. Package open-vm-tools has not been or will not be installed.";
 fi
 
 #Install qemu-guest-agent
 if systemd-detect-virt | grep kvm > /dev/null; then
 if rpm -q qemu-guest-agent > /dev/null; then
+  echo ""
   echo "You are running on KVM. Package qemu-guest-agent is already installed.";
 else echo "You are running on KVM. Package qemu-guest-agent will now be installed.";
   yum install qemu-guest-agent -y -q
 fi
 else
+ echo ""
  echo "You are not running on KVM. Package qemu-guest-agent has not been or will not be installed.";
 fi
 
 #Disable firewalld
+echo ""
+echo "Now disabling firewalld.";
 systemctl disable firewalld
 systemctl mask firewalld.service
 systemctl stop firewalld.service
